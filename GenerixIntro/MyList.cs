c5130baac84c28f23f84c6618d4d86f7bde8eb23
ksebil2301 <ksebiluyanik@gmail.com>
2021-01-27 23:50:27 +0300

    Add project files.

---
+++ GenerixIntro/MyList.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenerixIntro
+{
+    class MyList <T>
+    {
+        T[] İtems;
+        //constructor
+        public MyList()
+        {
+            İtems = new T[0];    
+        }
+        public void Add(T item)
+        {
+            T[] tempArray = İtems;
+            İtems = new T[İtems.Length + 1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                İtems[i] = tempArray[i];
+            }
+            İtems[İtems.Length - 1] = item;
+        }
+    }
+}
